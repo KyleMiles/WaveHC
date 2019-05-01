d7567a83c5bb8e8af64e620d4319b92a6224dfad
KyleMiles <krm504@nyu.edu>
2019-05-01 13:20:09 -0400

    Fix Spacing and Replace Bad Assumption

--- WaveHC.cpp
+++ WaveHC.cpp
@@ -16,147 +16,147 @@
  * You should have received a copy of the GNU General Public License
  * along with the Arduino WaveHC Library.  If not, see
  * <http://www.gnu.org/licenses/>.
- */
+*/
 
 /**
-\mainpage Arduino WaveHC Library
-<CENTER>Copyright &copy; 2009 by William Greiman
-</CENTER>
+  \mainpage Arduino WaveHC Library
+  <CENTER>Copyright &copy; 2009 by William Greiman
+  </CENTER>
 
-\section Intro Introduction
+  \section Intro Introduction
 
-WaveHC is an Arduino library for the Adafruit Wave Shield.  It can play
-uncompressed mono Wave(.WAV) files at sample rate up to 44.1 K samples per
-second. Only the high 12 bits of 16-bit files are used.  Audio files are read
-from an SD flash memory card.
+  WaveHC is an Arduino library for the Adafruit Wave Shield.  It can play
+  uncompressed mono Wave(.WAV) files at sample rate up to 44.1 K samples per
+  second. Only the high 12 bits of 16-bit files are used.  Audio files are read
+  from an SD flash memory card.
 
-Standard SD and high capacity SDHC flash memory cards are supported with
-FAT16 or FAT32 file systems.  The WaveHC only supports short FAT 8.3 names.
+  Standard SD and high capacity SDHC flash memory cards are supported with
+  FAT16 or FAT32 file systems.  The WaveHC only supports short FAT 8.3 names.
 
-WaveHC does not support MMC flash cards.
+  WaveHC does not support MMC flash cards.
 
-\section comment Bugs and Comments
+  \section comment Bugs and Comments
 
-If you wish to report bugs or have comments, send email to
-fat16lib@sbcglobal.net.
+  If you wish to report bugs or have comments, send email to
+  fat16lib@sbcglobal.net.
 
-\section SDcard SD/SDHC Cards
+  \section SDcard SD/SDHC Cards
 
-Arduinos access SD cards using the cards SPI protocol.  PCs, Macs, and
-most consumer devices use the 4-bit parallel SD protocol.  A card that
-functions well on A PC or Mac may not work well on the Arduino.
+  Arduinos access SD cards using the cards SPI protocol.  PCs, Macs, and
+  most consumer devices use the 4-bit parallel SD protocol.  A card that
+  functions well on A PC or Mac may not work well on the Arduino.
 
-Most cards have good SPI read performance but cards vary widely in
-how there SPI hardware interface is implemented.  Newer card require
-very fast rise times for SPI signals.  Version 1.0 of the Wave Shield
-may not work well with these cards.  Ladyada's improved Version 1.1
-works with almost all SD/SDHC cards.
+  Most cards have good SPI read performance but cards vary widely in
+  how there SPI hardware interface is implemented.  Newer card require
+  very fast rise times for SPI signals.  Version 1.0 of the Wave Shield
+  may not work well with these cards.  Ladyada's improved Version 1.1
+  works with almost all SD/SDHC cards.
 
-The default SPI clock rate is 8 Mhz.  It may be helpful on Version 1.0
-wave shields to reduce this to 4 Mhz. See SdReader::init() for details.
+  The default SPI clock rate is 8 Mhz.  It may be helpful on Version 1.0
+  wave shields to reduce this to 4 Mhz. See SdReader::init() for details.
 
-SanDisk cards generally have good performance in the Version 1.0 Wave Shield.
+  SanDisk cards generally have good performance in the Version 1.0 Wave Shield.
 
 
-\section WaveHCClass WaveHC Usage
+  \section WaveHCClass WaveHC Usage
 
-See Ladyada's excellent tutorial on using WaveHC:
+  See Ladyada's excellent tutorial on using WaveHC:
 
-http://www.ladyada.net/make/waveshield/libraryhc.html
+  http://www.ladyada.net/make/waveshield/libraryhc.html
 
-Also see the readme.txt file for instructions on installing WaveHC.
+  Also see the readme.txt file for instructions on installing WaveHC.
 
-Advanced users may need to edit the WavePinDefs.h file.
+  Advanced users may need to edit the WavePinDefs.h file.
 
-WaveHC uses a slightly restricted form of short file names.
-Only printable ASCII characters are supported. No characters with code point
-values greater than 127 are allowed.  Space is not allowed even though space
-was allowed in the API of early versions of DOS.
+  WaveHC uses a slightly restricted form of short file names.
+  Only printable ASCII characters are supported. No characters with code point
+  values greater than 127 are allowed.  Space is not allowed even though space
+  was allowed in the API of early versions of DOS.
 
-Short names are limited to 8 characters followed by an optional period (.)
-and extension of up to 3 characters.  The characters may be any combination
-of letters and digits.  The following special characters are also allowed:
+  Short names are limited to 8 characters followed by an optional period (.)
+  and extension of up to 3 characters.  The characters may be any combination
+  of letters and digits.  The following special characters are also allowed:
 
-$ % ' - _ @ ~ ` ! ( ) { } ^ # &
+  $ % ' - _ @ ~ ` ! ( ) { } ^ # &
 
-Short names are always converted to upper case and their original case
-value is lost.
+  Short names are always converted to upper case and their original case
+  value is lost.
 
-\section HowTo How to Format and Prepare SD Cards for WaveHC
+  \section HowTo How to Format and Prepare SD Cards for WaveHC
 
-WaveHC is optimized for contiguous files.  It will only play 16-bit
-44.1 K files if they are contiguous.  All files copied to a newly
-formatted card will be contiguous.  It is only possible to create
-a fragmented file if you delete a file from an SD and copy a larger
-file to the SD.
+  WaveHC is optimized for contiguous files.  It will only play 16-bit
+  44.1 K files if they are contiguous.  All files copied to a newly
+  formatted card will be contiguous.  It is only possible to create
+  a fragmented file if you delete a file from an SD and copy a larger
+  file to the SD.
 
-You should use a freshly formatted SD card for best performance.  FAT
-file systems become slower if many files have been created and deleted.
-This is because the directory entry for a deleted file is marked as deleted,
-but is not deleted.  When a file is opened, these entries must be scanned
-to find the file to be opened, a flaw in the FAT design.  Also files can
-become fragmented which causes reads to be slower.
+  You should use a freshly formatted SD card for best performance.  FAT
+  file systems become slower if many files have been created and deleted.
+  This is because the directory entry for a deleted file is marked as deleted,
+  but is not deleted.  When a file is opened, these entries must be scanned
+  to find the file to be opened, a flaw in the FAT design.  Also files can
+  become fragmented which causes reads to be slower.
 
-Microsoft operating systems support removable media formatted with a
-Master Boot Record, MBR, or formatted as a super floppy with a FAT Boot Sector
-in block zero.
+  Microsoft operating systems support removable media formatted with a
+  Master Boot Record, MBR, or formatted as a super floppy with a FAT Boot Sector
+  in block zero.
 
-Microsoft operating systems expect MBR formatted removable media
-to have only one partition. The first partition should be used.
+  Microsoft operating systems expect MBR formatted removable media
+  to have only one partition. The first partition should be used.
 
-Microsoft operating systems do not support partitioning SD flash cards.
-If you erase an SD card with a program like KillDisk, Most versions of
-Windows will format the card as a super floppy.
+  Microsoft operating systems do not support partitioning SD flash cards.
+  If you erase an SD card with a program like KillDisk, Most versions of
+  Windows will format the card as a super floppy.
 
-The best way to restore an SD card's format is to use SDFormatter
-which can be downloaded from:
+  The best way to restore an SD card's format is to use SDFormatter
+  which can be downloaded from:
 
-http://www.sdcard.org/consumers/formatter/
+  http://www.sdcard.org/consumers/formatter/
 
-SDFormatter aligns flash erase boundaries with file
-system structures which reduces write latency and file system overhead.
+  SDFormatter aligns flash erase boundaries with file
+  system structures which reduces write latency and file system overhead.
 
-SDFormatter does not have an option for FAT type so it may format
-small cards as FAT12.
+  SDFormatter does not have an option for FAT type so it may format
+  small cards as FAT12.
 
-After the MBR is restored by SDFormatter you may need to reformat small
-cards that have been formatted FAT12 to force the volume type to be FAT16.
+  After the MBR is restored by SDFormatter you may need to reformat small
+  cards that have been formatted FAT12 to force the volume type to be FAT16.
 
-If you reformat the SD card with an OS utility, choose a cluster size that
-will result in:
+  If you reformat the SD card with an OS utility, choose a cluster size that
+  will result in:
 
-4084 < CountOfClusters && CountOfClusters < 65525
+  4084 < CountOfClusters && CountOfClusters < 65525
 
-The volume will then be FAT16.
+  The volume will then be FAT16.
 
-If you are formatting an SD card on OS X or Linux, be sure to use the first
-partition. Format this partition with a cluster count in above range.
+  If you are formatting an SD card on OS X or Linux, be sure to use the first
+  partition. Format this partition with a cluster count in above range.
 
-\section  References References
+  \section  References References
 
-Adafruit Industries:
+  Adafruit Industries:
 
-http://www.adafruit.com/
+  http://www.adafruit.com/
 
-http://www.ladyada.net/make/waveshield/
+  http://www.ladyada.net/make/waveshield/
 
-The Arduino site:
+  The Arduino site:
 
-http://www.arduino.cc/
+  http://www.arduino.cc/
 
-For more information about FAT file systems see:
+  For more information about FAT file systems see:
 
-http://www.microsoft.com/whdc/system/platform/firmware/fatgen.mspx
+  http://www.microsoft.com/whdc/system/platform/firmware/fatgen.mspx
 
-For information about using SD cards as SPI devices see:
+  For information about using SD cards as SPI devices see:
 
-http://www.sdcard.org/developers/tech/sdcard/pls/Simplified_Physical_Layer_Spec.pdf
+  http://www.sdcard.org/developers/tech/sdcard/pls/Simplified_Physical_Layer_Spec.pdf
 
-The ATmega328 datasheet:
+  The ATmega328 datasheet:
 
-http://www.atmel.com/dyn/resources/prod_documents/doc8161.pdf
- 
- */  
+  http://www.atmel.com/dyn/resources/prod_documents/doc8161.pdf
+  
+*/  
 
 #include <string.h>
 #include <avr/interrupt.h>
@@ -165,8 +165,8 @@ http://www.atmel.com/dyn/resources/prod_documents/doc8161.pdf
 #include <WaveUtil.h>
 
 // verify program assumptions
-#if PLAYBUFFLEN != 256 && PLAYBUFFLEN != 512
-#error PLAYBUFFLEN must be 256 or 512
+#ifndef PLAYBUFFLEN
+#error PLAYBUFFLEN must be defined
 #endif // PLAYBUFFLEN
 
 WaveHC *playing = 0;
@@ -230,12 +230,12 @@ ISR(TIMER1_COMPA_vect) {
     playpos++;
   }
   
-#if DVOLUME
+ #if DVOLUME
   uint16_t tmp = (dh << 8) | dl;
   tmp >>= playing->volume;
   dh = tmp >> 8;
   dl = tmp;
-#endif //DVOLUME
+ #endif //DVOLUME
 
   // dac chip select low
   mcpDacCsLow();
@@ -268,9 +268,9 @@ ISR(TIMER1_COMPA_vect) {
   mcpDacCsHigh();
 
 }
+
 //------------------------------------------------------------------------------
 // this is the interrupt that fills the playbuffer
-
 ISR(TIMER1_COMPB_vect) {
 
   // turn off calling interrupt
@@ -293,11 +293,13 @@ ISR(TIMER1_COMPB_vect) {
     sdstatus = SD_END_FILE;
   }
 }
+
 //------------------------------------------------------------------------------
 /** create an instance of WaveHC. */
 WaveHC::WaveHC(void) {
   fd = 0;
 }
+
 //------------------------------------------------------------------------------
 /**
  * Read a wave file's metadata and initialize member variables.
@@ -329,10 +331,10 @@ uint8_t WaveHC::create(FatReader &f) {
     } fmt; // fmt data
   } buf;
   
-#if OPTIMIZE_CONTIGUOUS
+ #if OPTIMIZE_CONTIGUOUS
   // set optimized read for contiguous files
   f.optimizeContiguous();
-#endif // OPTIMIZE_CONTIGUOUS
+ #endif // OPTIMIZE_CONTIGUOUS
 
   // must start with WAVE header
   if (f.read(&buf, 12) != 12
@@ -383,7 +385,7 @@ uint8_t WaveHC::create(FatReader &f) {
   uint32_t clockRate = dwSamplesPerSec*Channels;
   uint32_t byteRate = clockRate*BitsPerSample/8;
   
-#if RATE_ERROR_LEVEL > 0
+ #if RATE_ERROR_LEVEL > 0
   if (clockRate > MAX_CLOCK_RATE
       || byteRate > MAX_BYTE_RATE) {
     putstring_nl("Sample rate too high!");
@@ -397,7 +399,7 @@ uint8_t WaveHC::create(FatReader &f) {
       return false;
     }
   }
-#endif // RATE_ERROR_LEVEL > 0
+ #endif // RATE_ERROR_LEVEL > 0
 
   fd = &f;
 
@@ -405,12 +407,13 @@ uint8_t WaveHC::create(FatReader &f) {
   isplaying = 0;
   remainingBytesInChunk = 0;
   
-#if DVOLUME
+ #if DVOLUME
   volume = 0;
-#endif //DVOLUME
+ #endif //DVOLUME
   // position to data
   return readWaveData(0, 0) < 0 ? false: true;
 }
+
 //------------------------------------------------------------------------------
 /*!
  * @brief Returns true if the player is paused else false.
@@ -422,6 +425,7 @@ uint8_t WaveHC::isPaused(void) {
   sei();
   return rtn;
 }
+
 //------------------------------------------------------------------------------
 /**
  * Pause the player.
@@ -432,6 +436,7 @@ void WaveHC::pause(void) {
   sei();
   fd->volume()->rawDevice()->readEnd(); // redo any partial read on resume
 }
+
 //------------------------------------------------------------------------------
 /**
  * Play a wave file.
@@ -479,6 +484,7 @@ void WaveHC::play(void) {
   // Enable timer interrupt for DAC ISR
   TIMSK1 |= _BV(OCIE1A);
 }
+
 //------------------------------------------------------------------------------
 /*! Read wave data.
  *
@@ -521,6 +527,7 @@ int16_t WaveHC::readWaveData(uint8_t *buff, uint16_t len) {
   if (ret > 0) remainingBytesInChunk -= ret;
   return ret;
 }
+
 //------------------------------------------------------------------------------
 /** Resume a paused player. */
 void WaveHC::resume(void) {
@@ -529,6 +536,7 @@ void WaveHC::resume(void) {
   if(isplaying) TIMSK1 |= _BV(OCIE1A);
   sei();
 }
+
 //------------------------------------------------------------------------------
 /**
  * Reposition a wave file.
@@ -552,6 +560,7 @@ void WaveHC::seek(uint32_t pos) {
   }
   sei();
 }
+
 //------------------------------------------------------------------------------
 /** Set the player's sample rate.
  *
@@ -568,6 +577,7 @@ void WaveHC::setSampleRate(uint32_t samplerate) {
   OCR1A = F_CPU / samplerate;
   sei();
 }
+
 //------------------------------------------------------------------------------
 /** Stop the player. */
 void WaveHC::stop(void) {
